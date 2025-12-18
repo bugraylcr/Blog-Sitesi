809e223be0ee8f7136ca89ccf5a4ad6eab4d67d7
buğra yolaçar <152706014+bugraylcr@users.noreply.github.com>
2025-12-18 05:14:11 +0300

    Eklemeler

--- Blog_Sitesi/resources/views/iletisim.blade.php
+++ Blog_Sitesi/resources/views/iletisim.blade.php
@@ -168,7 +168,7 @@
             <h2>BAUM</h2>
             <p>Bize ulaşmak için sosyal medya hesaplarımızı kullanabilirsiniz.</p>
             <div class="social-links">
-                <a href="https://facebook.com/atabaum" aria-label="Facebook">
+                <a href="https://www.facebook.com/bugra.yolacar/" aria-label="Facebook">
                     <svg viewBox="0 0 320 512"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06H296V6.26S259.5 0 225.36 0C141.09 0 89.09 54.42 89.09 153.12v74.22H0v92.66h89.09V512h107.78V288z"/></svg>
                 </a>
                 <a href="https://www.instagram.com/bugra_ylcr/" aria-label="Instagram">
